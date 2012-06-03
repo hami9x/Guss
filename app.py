d0f3e6999578917cca9eb805565e0c0ee67cf92c
Nguyễn Thành Hải <phaikawl@gmail.com>
2012-06-03 21:29:22 +0700

    trivial

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import homepage
 import authhandler
 import admin_user
 import admin_config
+import config
 
 tempconfig = {}
 tempconfig["webapp2_extras.sessions"] = {
-            "secret_key": "temporary_secret_key",
+            "secret_key": config.get_config("session_secret_key")
             }
 
 app = webapp2.WSGIApplication([
